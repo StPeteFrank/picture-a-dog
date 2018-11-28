27d041e339ba527fcf7c2ef84e4e19152b113813
Frank Diaz <diazfrankp@gmail.com>
2018-11-28 17:20:41 -0500

    Basic HTML and CSS

--- src/App.js
+++ src/App.js
@@ -11,11 +11,24 @@ class App extends Component {
           <h1>Dog Breed Finder 🐾</h1>
         </header>
         <main>
-          <div>
+          <form>
             <input type="text" placeholder="Enter Dog Breed" />
-          </div>
+          </form>
+          <ul>
+            <button class="germanshepherd">German Shepherd</button>
+            <button class="doberman">Doberman</button>
+            <button class="irish wolfhound">Irish Wolfhound</button>
+            <button class="rottweiler">Rottweiler</button>
+            <button class="rhodesian ridgeback">Rhodesian Ridgeback</button>
+            <button class="golden retriever">Golden Retriever</button>
+            <button class="scottish deerhound">Scottish Deerhound</button>
+            <button class="french bulldog">French Bulldog</button>
+            <button class="akita">Akita</button>
+            <button class="great dane">Great Dane</button>
+          </ul>
+
           <div>
-            <p>Dog Images Here</p>
+            <p>⬇️ Dog Images Go Here ⬇️</p>
           </div>
         </main>
       </div>
@@ -26,5 +39,11 @@ class App extends Component {
 export default App
 
 //What do you want this app to do:
+//Make a list of 10 hardcoded dogs show image
+
 //Enter a dog name in an input bar
 //Hit enter and dog image pops up
+
+//Study API
+//Practice API in Postman
+//
