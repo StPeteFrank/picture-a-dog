78efb21caaf680f981b2e7bb488761c5ff187cd4
Frank Diaz <diazfrankp@gmail.com>
2018-11-28 16:17:36 -0500

    Input field

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Dog Breed Finder 🐾</h1>
         </header>
+        <main>
+          <div>
+            <input type="text" placeholder="Enter Dog Breed" />
+          </div>
+          <div>
+            <p>Dog Images Here</p>
+          </div>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+//What do you want this app to do:
+//Enter a dog name in an input bar
+//Hit enter and dog image pops up
